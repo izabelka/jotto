c51640b34bb1ca9799d3c8ded4665268f2438ba6
izabelka <i.konstanciak@gmail.com>
2018-12-19 18:52:10 -0800

    Add bootstrap

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -19,13 +19,13 @@ const GuessedWords = (props) => {
     contents = (
       <div data-test="guessed-words">
         <h3>Guessed Words</h3>
-        <table>
-          <thead>
+        <table className="table table-sm">
+          <thead className="thead-light">
             <tr><th>Guess</th><th>Matching Letters</th></tr>
-            <tbody>
-              { guessedWordsRows }
-            </tbody>
           </thead>
+          <tbody>
+            { guessedWordsRows }
+          </tbody>
         </table>
       </div>
     )
