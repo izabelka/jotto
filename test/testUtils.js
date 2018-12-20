6a7ca594396a13340655bf7e60cbabff1dff02d6
izabelka <i.konstanciak@gmail.com>
2018-12-19 17:41:53 -0800

    extraxt enzyme setup

--- test/testUtils.js
+++ test/testUtils.js
@@ -5,5 +5,10 @@ export const findByTestAttr = (wrapper, val) => {
 }
 
 export const checkProps = (component, conformingProps) => {
-
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name);
+  expect(propError).toBeUndefined();
 }
