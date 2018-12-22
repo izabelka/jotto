4e921d9c983f54df6b39d20a176734e08c50d2e5
izabelka <i.konstanciak@gmail.com>
2018-12-21 17:12:19 -0800

    Add Thunk for `guessWords` piece of state + code refactoring

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,6 +3,6 @@ import ReduxThunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 export const middlewares = [ReduxThunk];
-const createStoreWithMiddleware = applyMiddleware(...middlewares)
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
 
 export default createStoreWithMiddleware(rootReducer);
